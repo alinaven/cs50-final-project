25f8128c299c965be0bcdc05420667713c2ef20a
JosephineD1995 <105046128+JosephineD1995@users.noreply.github.com>
2023-03-22 14:31:30 +0100

    Add logic for punctuation customer parameter
    Underscores worden vervangen door spaties en elk woord begint met een hoofdletter.

--- app.py
+++ app.py
@@ -1,7 +1,28 @@
 from flask import Flask, render_template
+import string
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/<customer>")
+def customerpage(customer):
+    if (customer == "intratuin" or customer == "het_oosten"):
+        # Replace underscore with space
+        customer = customer.replace("_", " ")
+
+        # Make first letter of each word uppercase
+        customer = string.capwords(customer)
+
+        return render_template("customer.html", customer=customer)
+    else: 
+        return render_template("error.html", text="Customer not available")
+
+@app.route("/<customer>/querytester")
+def querytester(customer):
+    if (customer == "intratuin" or customer == "het_oosten"):
+        return render_template("querytester.html", customer=customer)
+    else: 
+        return render_template("error.html", text="Customer not available")
