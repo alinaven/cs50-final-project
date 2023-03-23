1b280a110ec7a5549357cde6e0dda36180f428dd
alinaven <95190009+alinaven@users.noreply.github.com>
2023-03-23 16:18:52 +0100

    Refactor app.py / add make_friendly function

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-import string
+from helper import make_friendly
 
 app = Flask(__name__)
 
@@ -10,25 +10,13 @@ def index():
 @app.route("/<customer>")
 def customerpage(customer):
     if (customer == "intratuin" or customer == "het_oosten"):
-        # Replace underscore with space
-        customerCap = customer.replace("_", " ")
-
-        # Make first letter of each word uppercase
-        customerFriendly = string.capwords(customerCap)
-
-        return render_template("customer.html", customerFriendly=customerFriendly, customer=customer)
+        return render_template("customer.html", customerFriendly=make_friendly(customer), customer=customer)
     else: 
         return render_template("error.html", text="Customer not available")
 
 @app.route("/<customer>/querytester")
 def querytester(customer):
     if (customer == "intratuin" or customer == "het_oosten"):
-        # Replace underscore with space
-        customerCap = customer.replace("_", " ")
-
-        # Make first letter of each word uppercase
-        customerFriendly = string.capwords(customerCap)
-
-        return render_template("querytester.html", customerFriendly=customerFriendly, customer=customer)
+        return render_template("querytester.html", customerFriendly=make_friendly(customer), customer=customer)
     else: 
         return render_template("error.html", text="Customer for querytester not available")
