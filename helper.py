5031768e0829ffcff5a44bc798f792d80d5d30d3
JosephineD1995 <105046128+JosephineD1995@users.noreply.github.com>
2023-03-23 16:23:34 +0100

    Revert "Merge branch 'main' of https://github.com/alinaven/cs50-final-project"
    This reverts commit c130da88ec1c28998e49962f01a3dbc774853e7f, reversing
    changes made to 612b56fd39c8c60a762b03d4aa0cf2a103c728ae.

--- helper.py
+++
@@ -1,7 +0,0 @@
-import string
-
-def make_friendly(customer):
-    # Replace underscore with space
-    # Make first letter of each word uppercase
-    customerFriendly = string.capwords(customer.replace("_", " "))
-    return customerFriendly
