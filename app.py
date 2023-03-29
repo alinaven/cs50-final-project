332290006fc24007215461a44f7e012d998dee2a
JosephineD1995 <105046128+JosephineD1995@users.noreply.github.com>
2023-03-29 15:03:32 +0200

    Added database logic to customer routing
    Logic to create and edit database is now within the python function which is not desired, but made this work.

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from helper import make_friendly
+import sqlite3
 
 app = Flask(__name__)
 
@@ -9,7 +10,20 @@ def index():
 
 @app.route("/<customer>")
 def customerpage(customer):
-    if (customer == "intratuin" or customer == "het_oosten"):
+    # Retrieve customer suffix's from database
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = lambda cursor, row: row[0]
+
+    # TO DO: MIGRATE OUT THE CODE LINES WHERE WE ADD/INSERT DATA TO THE DATABASE WITHIN THESE PYTHON FUNCTION!
+    conn.execute('DROP TABLE customers')
+    conn.execute('CREATE TABLE customers(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, suffix TEXT NOT NULL)')
+    conn.execute('INSERT INTO customers (name, suffix) VALUES (?, ?)', ["Intratuin", "intratuin"])
+    conn.execute('INSERT INTO customers (name, suffix) VALUES (?, ?)', ["Het Oosten", "het_oosten"])
+    cursor = conn.execute('SELECT suffix FROM customers')
+    print(cursor)
+    data = cursor.fetchall()
+    print(data)
+    if customer in data:
         return render_template("customer.html", customerFriendly=make_friendly(customer), customer=customer)
     else: 
         return render_template("error.html", text="Customer not available")
--- db_editor.py
+++ db_editor.py
@@ -1,14 +1,20 @@
 import sqlite3
 
 conn = sqlite3.connect('database.db')
+conn.row_factory = lambda cursor, row: row[0]
 print("Opened succesfully")
 
-#conn.execute('CREATE TABLE customers(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL)')
+conn.execute('DROP TABLE customers')
+conn.execute('CREATE TABLE customers(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, suffix TEXT NOT NULL)')
+conn.execute('INSERT INTO customers (name, suffix) VALUES (?, ?)', ["Intratuin", "intratuin"])
+conn.execute('INSERT INTO customers (name, suffix) VALUES (?, ?)', ["Het Oosten", "het_oosten"])
 
-intratuin = "Intratuin"
-hetoosten = "Het Oosten"
-conn.execute('INSERT INTO customers (name) VALUES (?)', [intratuin])
-conn.execute('INSERT INTO customers (name) VALUES (?)', [hetoosten])
+cursor = conn.execute('SELECT suffix FROM customers')
+data = cursor.fetchall()
+test = "intratuin"
+print(data)
 
-data = conn.execute('SELECT * FROM customers')
-print(data.fetchall())
+if test in data:
+    print("success!")
+
+conn.close()
