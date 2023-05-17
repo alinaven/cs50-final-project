3112fbfc57512f7d4377ee70f2fe25f55c2b4468
JosephineD1995 <105046128+JosephineD1995@users.noreply.github.com>
2023-05-17 15:44:10 +0200

    Customer API in database

--- schema.sql
+++ schema.sql
@@ -1,4 +1,4 @@
 DROP TABLE IF EXISTS customers;
-CREATE TABLE customers (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, suffix TEXT NOT NULL);
-INSERT INTO customers (name, suffix) VALUES ("Intratuin", "intratuin");
-INSERT INTO customers (name, suffix) VALUES ("Het Oosten", "het_oosten");
+CREATE TABLE customers (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, suffix TEXT NOT NULL, url TEXT NOT NULL, source TEXT NOT NULL);
+INSERT INTO customers (name, suffix, url, source) VALUES ("Intratuin", "intratuin", 'intratuin-api', 'data_intratuin.txt');
+INSERT INTO customers (name, suffix, url, source) VALUES ("Het Oosten", "het_oosten", 'oosten-api', 'data_het_oosten.txt');
